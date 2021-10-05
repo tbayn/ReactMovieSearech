99e44efd70cb7523c96fd83a8a80e16e33d76e29
tbayn <79238799+tbayn@users.noreply.github.com>
2021-10-05 10:40:33 -0400

    Add files via upload

---
+++ starwars.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+function StarWars () {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    const [toggle, setToggle] = useState (false)
+
+    useEffect(() => {
+        fetch('http://www.omdbapi.com/?apikey=2469208&t=Star+Wars+&plot=full&r=json')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                setIsLoaded(true)
+                setItems(result)
+                console.log(result.Title)
+            },
+            (error) => {
+                setIsLoaded(true)
+                setError(error)
+            })
+    }, [])
+
+    function showPoster () {
+        if(toggle === false){
+            document.getElementById('SW').style.display = "block"
+            setToggle(true)
+        }
+        else {
+            document.getElementById('SW').style.display = "none"
+            setToggle(false)
+        }
+       }
+
+
+    return (
+        <article className={"A1"}>
+                    <h2>{items.Title}</h2>
+                    <br/>
+                    <img alt = "" src = {items.Poster}></img>
+                    <p className ="plot" id ={"SW"}> {items.Plot}</p>
+                    <button onClick ={showPoster}>Clickable</button>
+                </article>
+    )
+}
+
+export default StarWars
